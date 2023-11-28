5765e71b595e515f6f4e4e8faa5f96ae897b69a0
William Lin <uslwr314@gmail.com>
2023-11-28 12:44:58 -0800

    fix grade.sh

--- grade.sh
+++ grade.sh
@@ -8,6 +8,30 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then
+echo 'file found'
+else
+echo 'file not found'
+fi
+
+if [[ -f student-submission/ListExample.java ]]
+then
+echo 'file found'
+else
+echo 'file not found'
+fi
+
+cp TestListExamples.java grading-area/
+cp student-submission/* grading-area/
+cp lib/* grading-area/
+echo 'moved files'
+
+echo 'compiling files'
+cd grading-area
+javac -cp .:hamcrest-core-1.3.jar:junit-4.13.2.jar *.java
+java -cp .:hamcrest-core-1.3.jar:junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
